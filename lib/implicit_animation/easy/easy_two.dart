24d4997fba017f6ae70f4825d232c7c33b16f667
PrinceLeBon <jeffersonhoundjo@gmail.com>
2023-07-11 20:36:02 +0100

    Exercice 2 (easy) done

---
+++ lib/implicit_animation/easy/easy_two.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class EasyTwo extends StatefulWidget {
+  const EasyTwo({super.key});
+
+  @override
+  State<EasyTwo> createState() => _EasyTwoState();
+}
+
+class _EasyTwoState extends State<EasyTwo> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: [
+                  AnimatedPositioned(
+                    duration: const Duration(seconds: 3),
+                    top: pressed ? 120 : 5,
+                    left: pressed ? 120 : 5,
+                    child: Container(
+                      width: 75,
+                      height: 75,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    pressed = true;
+                  });
+                },
+                child: const Text("Move")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    pressed = false;
+                  });
+                },
+                child: const Text("Reset")),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'implicit_animation/easy/easy_one.dart';
+import 'implicit_animation/easy/easy_two.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
-      home: const EasyOne(),
+      home: const EasyTwo(),
     );
   }
 }

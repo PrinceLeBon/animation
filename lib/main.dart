3e46df3120f89a76a9570920b454559b6cd1ef87
PrinceLeBon <jeffersonhoundjo@gmail.com>
2023-10-03 17:26:47 +0100

    Shimmer effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'downald_button.dart';
+import 'loading_effect.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
-      home: const ExampleCupertinoDownloadButton(),
+      home: const ExampleUiLoadingAnimation(),
     );
   }
 }

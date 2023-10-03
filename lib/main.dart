cdfcccf458a9c49563596e7b454cecef1e6d13f6
PrinceLeBon <jeffersonhoundjo@gmail.com>
2023-10-03 16:38:27 +0100

    Page Transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:animation/page_transition.dart';
 import 'package:flutter/material.dart';
-import 'hero.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
-      home: const RadialExpansionDemo(),
+      home: const PageTransition(),
     );
   }
 }
